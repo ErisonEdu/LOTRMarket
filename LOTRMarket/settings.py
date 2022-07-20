1d9ac5441833e84501e5873c9d1a3b0f38caf0f4
Erison <erison.pimentao@gmail.com>
2022-07-20 20:24:44 -0300

    chore: add postgres database

--- LOTRMarket/settings.py
+++ LOTRMarket/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'LOTRMarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lotrmoarket',
+        'USER': 'postgres',
+        'PASSWORD': 'protectArda123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
