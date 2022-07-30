096209dfdb940b92e8add98475fb39ff236470c6
Erison <erison.pimentao@gmail.com>
2022-07-30 01:09:23 -0300

    feat: add dwarven market route

--- LOTRMarket/dwarves_market/urls.py
+++ LOTRMarket/dwarves_market/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import dwarven_market_view
 
 urlpatterns = [
-    path("elven_market/", dwarven_market_view, name="home"),
+    path("dwarven_market/", dwarven_market_view, name="home"),
 ]
--- LOTRMarket/urls.py
+++ LOTRMarket/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path("", include("LOTRMarket.humans_market.urls")),
     path('', include("LOTRMarket.elves_market.urls")),
+    path('', include("LOTRMarket.dwarves_market.urls"))
 ]
 
