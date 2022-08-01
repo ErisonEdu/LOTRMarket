e0d1bab7461197ba93afa500792e2fb43e4beb61
Erison <erison.pimentao@gmail.com>
2022-08-01 00:20:09 -0300

    feat: add new human weapon class

--- LOTRMarket/humans_market/models.py
+++ LOTRMarket/humans_market/models.py
@@ -9,3 +9,12 @@ class HumanItem(models.Model):
 
     def __str__(self):
         return self.name
+
+class HumanWeapon(models.Model):
+    name = models.CharField(max_length=100)
+    categories = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return self.description
