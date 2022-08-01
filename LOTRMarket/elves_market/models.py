a5432141f3c96a0ce2694e6b0c466453e774a20b
Erison <erison.pimentao@gmail.com>
2022-08-01 00:20:42 -0300

    feat: add new elven weapon class

--- LOTRMarket/elves_market/models.py
+++ LOTRMarket/elves_market/models.py
@@ -7,3 +7,12 @@ class ElvenItem(models.Model):
 
     def __str__(self):
         return self.name
+
+class ElvenHumanWeapon(models.Model):
+    name = models.CharField(max_length=100)
+    categories = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return self.description
